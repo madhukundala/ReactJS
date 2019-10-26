a0eeb2961d9400fa7b7d1375d06831256a67acd2
Madhu Kundala <madhu.kundala@gmail.com>
2019-10-26 14:25:41 +1100

    implementation of grocery features .

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -32,6 +32,7 @@ class ProductRow extends React.Component {
                 <div className="row table-striped">
                 <input type="text" defaultValue={product.productName} ></input>
                 <input type="text" defaultValue={product.productCategory}></input>
+                 <input type="text" defaultValue={product.productValue}></input>
                 <button class="btn btn-secondary" onClick= {(event) => {event.preventDefault(); updateProductAction(product) } }>Update</button>
                 <button class="btn btn-danger" onClick= {(event) => {event.preventDefault(); deleteProductAction(product.id) } }>Delete</button>
                 </div>
